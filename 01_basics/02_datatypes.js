52e07a9df0e43377bfa465644ef27984fd2d909e
Anuj24680 <anujbadgaiya044@gmail.com>
2025-11-16 14:15:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all the JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not a browser
+
+console.log(3 + 3)
+console.log("Anuj Badgaiya")
+console.log("Ankit Badgaiya")
+
+let name = "Anuj Badgaiya"
+let age = 18
+let isloggedIn = false
+let state;
+console.log(state)
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof 18); // number
