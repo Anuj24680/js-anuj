63fa234cf9c817148c21841dd26705c26f6ee71b
Anuj24680 <anujbadgaiya044@gmail.com>
2025-11-16 14:35:39 +0000

    conversions

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+let score = "88"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInString);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isloggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// " " => false
+// "Anuj" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
