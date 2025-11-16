f171a275c5efcf56dccb4e3064df048d59e36b95
Anuj24680 <anujbadgaiya044@gmail.com>
2025-11-16 13:31:59 +0000

    push to my repo

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144582
+let accountEmail = "anujbadgaiya044@gmail.com"
+var accountPassword = "12345"
+accountCity = "Madhya Pradesh"
+let accounstate;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
